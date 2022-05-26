66688c18428de5cba7d10a48aa4969bdbeef6f26
Walter <walter.fiterer@web.de>
2022-05-26 15:48:33 +0200

    kopiert

--- Aufgabe3/gta_v3/public/javascripts/geotagging.js
+++ Aufgabe3/gta_v3/public/javascripts/geotagging.js
@@ -9,25 +9,17 @@
 // Try to find this output in the browser...
 console.log("The geoTagging script is going to start...");
 
-
-/**
- * TODO: 'updateLocation'
- * A function to retrieve the current location and update the page.
- * It is called once the page has been fully loaded.
- */
-
 function updateLocation(){
     LocationHelper.findLocation(function(helper) {
-         document.getElementById("tag_latitude").value = helper.latitude;
-         document.getElementById("tag_longitude").value = helper.longitude;
-         document.getElementById("discovery_latitude_input").value = helper.latitude;
-         document.getElementById("discovery_longtitude_input").value = helper.longitude;
+         document.getElementById("latitude").value = helper.latitude;
+         document.getElementById("longitude").value = helper.longitude;
+         document.getElementById("discovery_latitude").value = helper.latitude;
+         document.getElementById("discovery_longtitude").value = helper.longitude;
 
-
-         let mapManager=new MapManager("EussyP3bKYyMVPyfB8Y46Ng5VVQfBRyY");
+         let mapManager=new MapManager("XCbGiqL4T1sCHHoOW44CjYAeZg15xNPo");
          let mapQuestUrl=mapManager.getMapUrl(helper.latitude, helper.longitude, [], 18);
          document.getElementById("mapView").src=mapQuestUrl;
-         //comment
+         
          
      });
 }
@@ -41,5 +33,16 @@ document.addEventListener("DOMContentLoaded", () => {
     added the new created element in the unordered list
     */
     updateLocation();
+    /*
+    document.getElementById("addTag").addEventListener("click", function () {
+        var ul = document.getElementById("discovery__results_unorderedList");
+        var li = document.createElement("li");
+        let name = document.querySelector("#name").value;
+        let hash = document.querySelector("#hashtag").value;
+        let latitude = document.querySelector("#latitude").value;
+        let longitude = document.querySelector("#longitude").value;
+        li.appendChild(document.createTextNode(name + " (" + latitude +", " + longitude + ") " + hash));
+        ul.appendChild(li);
+    });*/
        
 });
