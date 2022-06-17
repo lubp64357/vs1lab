2368395c4283005217278caf07d3fa5b02da804b
Walter <walter.fiterer@web.de>
2022-06-17 14:41:29 +0200

    Aufgabe 3 erledigt

--- Aufgabe3/gta_v3/models/geotag.js
+++ Aufgabe3/gta_v3/models/geotag.js
@@ -10,20 +10,48 @@
  * GeoTag objects should contain at least all fields of the tagging form.
  */
  class GeoTag {
-    name = "";
-    latitude = 0;
-    longitude = 0;
-    hashtag = "";
+    #name = "";
+    #latitude = 0;
+    #longitude = 0;
+    #hashtag = "";
 
     constructor(name, latitude, longitude, hashtag) {
-        this.name = name;
-        this.latitude = latitude;
-        this.longitude = longitude;
-        this.hashtag = hashtag;
+        this.#name = name;
+        this.#latitude = latitude;
+        this.#longitude = longitude;
+        this.#hashtag = hashtag;
+    }
+    
+    /*In JavaScript, the JSON. stringify() function looks for functions named toJSON 
+    in the object being serialized. If an object has a toJSON function, JSON. stringify() calls toJSON() 
+    and serializes the return value from toJSON() instead.*/
+    toJSON() {
+        return {
+            name: this.#name,
+            latitude: this.#latitude,
+            longitude: this.#longitude
+        }
+    }
+
+    get name() {
+        return this.#name;
     }
 
 
-}
+    get latitude() {
+        return this.#latitude;
+    }
 
+
+    get longitude() {
+        return this.#longitude;
+    }
+
+
+    get hashtag() {
+        return this.#hashtag;
+    }
+
+}
 
 module.exports = GeoTag;
